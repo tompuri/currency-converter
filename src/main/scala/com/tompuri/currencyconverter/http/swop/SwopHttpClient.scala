86c0d83ef806132e628e909e9eb67752e7a01cd6
Jaakko Tompuri <182629584+tompuri@users.noreply.github.com>
2025-01-15 19:31:00 +0200

    feat: add logging to http client

--- src/main/scala/com/tompuri/currencyconverter/http/swop/SwopHttpClient.scala
+++ src/main/scala/com/tompuri/currencyconverter/http/swop/SwopHttpClient.scala
@@ -3,6 +3,7 @@ package com.tompuri.currencyconverter.http.swop
 import com.tompuri.currencyconverter.error.models.internal.InternalApiError
 import com.tompuri.currencyconverter.error.models.internal.InternalApiError.{DeserializationError, HttpError, NetworkError}
 import com.tompuri.currencyconverter.http.swop.models.response.RatesResponse
+import org.slf4j.LoggerFactory
 import sttp.client3.circe.*
 import sttp.client3.{Response, SttpBackend, UriContext, basicRequest}
 
@@ -10,6 +11,9 @@ import scala.concurrent.ExecutionContext.Implicits.global
 import scala.concurrent.Future
 
 class SwopHttpClient(apiKey: String, host: String, backend: SttpBackend[Future, Any]) {
+
+  val logger = LoggerFactory.getLogger(getClass)
+
   def rates(
       baseCurrency: String,
       quoteCurrency: String
@@ -27,12 +31,15 @@ class SwopHttpClient(apiKey: String, host: String, backend: SttpBackend[Future, 
         case Response(Left(error), statusCode, _, _, _, _) =>
           statusCode.code match {
             case code if code >= 400 && code <= 599 =>
+              logger.error(s"Failed to fetch rates for $baseCurrency/$quoteCurrency", error)
               Left(HttpError(error.toString, code))
             case _ =>
+              logger.error(s"Failed to fetch rates for $baseCurrency/$quoteCurrency", error)
               Left(DeserializationError(error.toString))
           }
       }
       .recover { case e: Exception =>
+        logger.error(s"Failed to fetch rates for $baseCurrency/$quoteCurrency", e)
         Left(NetworkError(e.getMessage))
       }
   }
